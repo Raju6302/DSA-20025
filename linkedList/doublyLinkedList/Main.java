56f7863dbe43f557ff39311a80b6c9df7c7480bb
Raju6302 <jeripitivijayraj@gmail.com>
2025-03-22 21:59:23 +0530

    code fix for linkedlist

--- linkedList/doublyLinkedList/Main.java
+++ linkedList/doublyLinkedList/Main.java
@@ -8,18 +8,18 @@ public class Main {
 
     myDLL.prepend(1);
 
-   System.out.println(myDLL.removeFirst().value);
+    System.out.println("removed First: " + " " + myDLL.removeFirst().value);
 
-    System.out.println(myDLL.removeLast().value);
+    System.out.println("removed last: " + " " + myDLL.removeLast().value);
+    System.out.println("removed last: " + " " + myDLL.removeLast().value);
 
-   System.out.println(myDLL.get(2).value);
+    System.out.println(myDLL.get(2).value);
 
-  System.out.println(myDLL.set(99, 3));
+    System.out.println(myDLL.set(99, 3));
 
-  myDLL.insert(100, 1);
+    myDLL.insert(100, 1);
 
-  myDLL.remove(1);
-
+    System.out.println("removed: " + " " + myDLL.remove(0));
 
     myDLL.getHead();
     myDLL.getTail();
